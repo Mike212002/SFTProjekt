172f6a082ef53d706cd81696b6eb022e921629ac
Giantfire <Giantfire@4fd9b3e4-9eb6-4216-89a0-8ad9c638f75f>
2024-02-08 12:06:31 +0000

    Beispiel variablen/daten abgeändert

--- SFTProjekt/src/main/DatenbankZuTxt.java
+++ SFTProjekt/src/main/DatenbankZuTxt.java
@@ -20,9 +20,9 @@ import java.sql.SQLException;
 
 public class DatenbankZuTxt {
 
-    private static final String DB_URL = "jdbc:mysql://localhost:3306/deineDatenbank";
-    private static final String USER = "deinBenutzername";
-    private static final String PASSWORD = "deinPasswort";
+    private static final String DB_URL = "jdbc:mysql://mike007.lima-db.de:3306/db_427829_1";
+    private static final String USER = "USER427829";
+    private static final String PASSWORD = "Milka123mo";
 
     public static void main(String[] args) {
         // Hier wird der Code für die GUI generiert (z. B. Buttons, ActionListener).
@@ -39,7 +39,7 @@ public class DatenbankZuTxt {
             try (BufferedWriter writer = new BufferedWriter(new FileWriter("datenbankdaten.txt"))) {
                 while (resultSet.next()) {
                     // Annahme: Du hast zwei Spalten in der Tabelle (Spalte1 und Spalte2).
-                    String data = resultSet.getString("Spalte1") + "\t" + resultSet.getString("Spalte2");
+                    String data = resultSet.getString("BetriebsID") + "\t" + resultSet.getString("Sterne");
                     writer.write(data);
                     writer.newLine();
                 }
